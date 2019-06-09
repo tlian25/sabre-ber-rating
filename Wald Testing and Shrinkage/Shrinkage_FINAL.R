a1dca2473b08e0e104e46d2495314d0d40b0c139
TXL <tlian25@yahoo.com>
2019-06-09 17:21:27 -0500

    BER

--- Wald Testing and Shrinkage/Shrinkage_FINAL.R
+++ Wald Testing and Shrinkage/Shrinkage_FINAL.R
@@ -2,7 +2,7 @@
 # Wald Statistic/P-Value Function
 
 wald.func <- function(B,V,R,df){
-  wald.stat <- t(R%*%B)%*%solve(R%*%V%*%t(R))%*%(R%*%B)
+  wald.stat <- t(R%*%B)%*%inv(R%*%V%*%t(R))%*%(R%*%B)
   wald.pvalue <- pchisq(wald.stat,df,lower.tail = FALSE)
   w <- cbind(wald.stat,wald.pvalue)
   return(w)
@@ -19,26 +19,24 @@ wald.func <- function(B,V,R,df){
 null.wald.test <- function(reg, covar){
   
   n <- as.numeric(length(reg$coefficients))
-  m <- n/4
-  w <- matrix(0,4,2,byrow = T)
+  m <- ((n-4)/4)
+  w <- matrix(0,1,2,byrow = T)
   colnames(w) <- c("Wald.Stat","Wald.p.Value")
   
   B <- reg$coefficients
   #V <- cluster.vcov(reg,cluster=dt$Half_Inning)
   V <- covar
+  R <- matrix(0,(n-4),n,byrow = T)
+  df <- (n-4)
   
-  df <- 23
   for(i in 1:4){
-    R <- matrix(0,(m-1),m,byrow = T)
-    B_i <- B[(((i-1)*24)+(1:24))]
-    V_i <- V[(((i-1)*24)+(1:24)),(((i-1)*24)+(1:24))]
-    for(j in 1:(m-1)){
-      R[j,j] <- 1
-      R[j,j+1] <- -1
+    for(j in 1:m){
+      R[j+(23*(i-1)),j+(24*(i-1))] <- 1
+      R[j+(23*(i-1)),j+(24*(i-1))+1] <- -1
     }
-    w[i,] <- wald.func(B_i,V_i,R,df)
-    
   }
+  
+  w[1,] <- wald.func(B,V,R,df)
   
   return(w)
 }
@@ -51,7 +49,6 @@ null.wald.test <- function(reg, covar){
 
 shrinkage <- function(reg, covar) {
   reg_wald <- null.wald.test(reg, covar)
-  h <- as.numeric(length(reg_wald))/2
   
   #Number of coefficients
   k <- as.numeric(length(reg$coefficients))
@@ -70,14 +67,13 @@ shrinkage <- function(reg, covar) {
     }
     delta[i,1] <- delta[i,1] - k
   }
+
   
   
   #Create Alpha Matrix
   alpha <- matrix(0,k,1,byrow = T)
-  for (j in 1:h){
-    for (i in 1:(k/4)) {
-      alpha[i+(24*(j-1)),1] <- (reg_wald[h,1]-(k-1))/(reg_wald[h,1]+delta[i+(24*(j-1)),1])
-    }
+  for (i in 1:k) {
+    alpha[i,1] <- (reg_wald[1,1]-(k-1))/(reg_wald[1,1]+delta[i,1])
   }
   
   #Create output Matrix
@@ -95,10 +91,11 @@ shrinkage <- function(reg, covar) {
   vl.shrink[25:48] = vl.shrink[1:24] + vl.shrink[25:48]
   # Add values of 0 for end inning states
   vl.shrink = c(vl.shrink, "end inningAL" = 0, "end inningNL" = 0, "end inning" = 0, 
-         "end inning:park_factor" = 0, "end inning:FIP" = 0)
-  
+                "end inning:park_factor" = 0, "end inning:FIP" = 0)
   
   return(list("wald" = reg_wald, 
               "coef.shrink" = shrunk, 
               "vl.shrink" = vl.shrink))
+  
+  
 }
