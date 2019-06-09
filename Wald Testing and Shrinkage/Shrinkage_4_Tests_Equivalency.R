f52b4dbf5a8b89a39b745303e25a91b2efe1d2fc
TXL <tlian25@yahoo.com>
2019-06-09 13:23:43 -0500

    Shrinkage

--- Wald Testing and Shrinkage/Shrinkage_4_Tests_Equivalency.R
+++ Wald Testing and Shrinkage/Shrinkage_4_Tests_Equivalency.R
@@ -2,7 +2,7 @@
 # Wald Statistic/P-Value Function
 
 wald.func <- function(B,V,R,df){
-  wald.stat <- t(R%*%B)%*%inv(R%*%V%*%t(R))%*%(R%*%B)
+  wald.stat <- t(R%*%B)%*%solve(R%*%V%*%t(R))%*%(R%*%B)
   wald.pvalue <- pchisq(wald.stat,df,lower.tail = FALSE)
   w <- cbind(wald.stat,wald.pvalue)
   return(w)
@@ -16,7 +16,7 @@ wald.func <- function(B,V,R,df){
 # interactions of FIP, park_factor, league) as separate
 # Hard-coded to cluster on data table dt for variable Half_Inning
 
-null.wald.test <- function(reg){
+null.wald.test <- function(reg, covar){
   
   n <- as.numeric(length(reg$coefficients))
   m <- n/4
@@ -24,7 +24,8 @@ null.wald.test <- function(reg){
   colnames(w) <- c("Wald.Stat","Wald.p.Value")
   
   B <- reg$coefficients
-  V <- cluster.vcov(reg,cluster=dt$Half_Inning)
+  #V <- cluster.vcov(reg,cluster=dt$Half_Inning)
+  V = covar
   
   df <- 23
   for(i in 1:4){
@@ -48,8 +49,8 @@ null.wald.test <- function(reg){
 #---------------------------------------------------------------
 # Shrinkage Estimation Function
 
-shrinkage <- function(reg) {
-  reg_wald <- null.wald.test(reg)
+shrinkage <- function(reg, covar) {
+  reg_wald <- null.wald.test(reg, covar)
   h <- as.numeric(length(reg_wald))/2
   
   #Number of coefficients
@@ -84,5 +85,20 @@ shrinkage <- function(reg) {
   shrunk <- cbind((coefs*alpha),alpha,coefs)
   colnames(shrunk) <- c("new","alpha","old")
   
-  return (shrunk)
+  vl.shrink = reg$coefficients * alpha[,1]
+  names(vl.shrink) = names(reg$coefficients) %>%
+    sapply(function(s) gsub("position_before", "", s)) %>%
+    sapply(function(s) gsub(":league", "", s)) %>%
+    sapply(function(s) if_else(grepl("*]$", s), paste0(s, "AL"), s))
+  
+  # Add base states to NL adjustments for total NL effect
+  vl.shrink[25:48] = vl.shrink[1:24] + vl.shrink[25:48]
+  # Add values of 0 for end inning states
+  vl.shrink = c(vl.shrink, "end inningAL" = 0, "end inningNL" = 0, "end inning" = 0, 
+         "end inning:park_factor" = 0, "end inning:FIP" = 0)
+  
+  
+  return(list("wald" = reg_wald, 
+              "coef.shrink" = shrunk, 
+              "vl.shrink" = vl.shrink))
 }
